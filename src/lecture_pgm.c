b5a4b08dd328b1dd83d07abffccaa908c2b3c2aa
Auger <arthur.auger@grenoble-inp.org>
2022-05-03 01:48:59 +0200

    finito passage du fichier en argument

--- src/lecture_pgm.c
+++ src/lecture_pgm.c
@@ -3,16 +3,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 #include <lecture_pgm.h>
 
 void process_file(struct image_pgm *pgm, const char *file_name) {
     //Ouvre le fichier en mode lecture binaire
-    printf("tacos \n");
     FILE *file = fopen(file_name, "rb");
-    printf("tacos \n");
     //Lecture du type de fichier
     fscanf(file, "%s", pgm->type_pgm);
-    printf("tacos \n");
     printf("%s \n", pgm->type_pgm);
     //Lecture des dimensions de l'image
     fscanf(file, "%d %d", &(pgm->width), &(pgm->height));
@@ -46,9 +44,13 @@ void affiche_details_image(struct image_pgm *pgm, const char * file_name) {
     }
     fclose(file);
 }
-int main (void) {
-    struct image_pgm *pgm = malloc(sizeof(struct image_pgm));
-    char *file_name = "images/invader.pgm";
-    process_file(pgm, file_name);
-    affiche_details_image(pgm, file_name);
+int main (int argc, char **argv) {
+    if (argc == 2) {
+        struct image_pgm *pgm = malloc(sizeof(struct image_pgm));
+        char *file_name = argv[1];
+        process_file(pgm, file_name);
+        affiche_details_image(pgm, file_name);
+    } else {
+        printf("Il faut passer en paramètre le nom d'un fichier image valide \n");
+    }
 }
