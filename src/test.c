654c89a5d0f66811910aca62fe1e0e0597ddc2aa
Cedric Lassalle <lassallc@ensipc508.ensimag.fr>
2022-05-03 16:33:54 +0200

    convert_to_ycbcr_passed

--- src/test.c
+++ src/test.c
@@ -4,23 +4,20 @@
 #include <stdbool.h>
 #include <lecture_pgm.h>
 #include <MCU.h>
+#include <convert_to_YCbCr.h>
 
 
 
 int main (int argc, char **argv) {
     if (argc == 2) {
-        printf("On est ici");
         struct image_pgm *pgm = malloc(sizeof(struct image_pgm));
         char *file_name = argv[1];
         process_file(pgm, file_name);
         affiche_details_image(pgm, file_name);
-        printf("On rentre dans mcu\n");
         struct image_mcu *img_mcu = decoupe_mcu_8x8(pgm);
-        printf("On affiche image_mcu\n");
         affiche_img_mcu(img_mcu);
-        //struct image_YCbCr *p_ycbcr = convert_YCbCr(img_mcu);
-        printf("On est sortie de la création\n");
-        //afficher_YCbCr(p_ycbcr);
+        struct image_YCbCr *p_ycbcr = convert_YCbCr(img_mcu);
+        afficher_YCbCr(p_ycbcr);
     } else {
         printf("Il faut passer en paramètre le nom d'un fichier image valide \n");
     }
