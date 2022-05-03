c093c112c5177be07f60ee39e79801f410463ad6
Auger <arthur.auger@grenoble-inp.org>
2022-05-03 02:29:47 +0200

    début du header

---
+++ include/header.h
@@ -0,0 +1,11 @@
+#ifndef _HEADER_H
+#define _HEADER_H
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+
+struct jpeg create_struct_header();
+
+#endif
---
+++ src/header.c
@@ -0,0 +1,16 @@
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <jpeg_writer.h>
+#include <header.h>
+
+struct jpeg write_header_file(const char* ppm_file, ) {
+    struct jpeg *img_jpeg = jpeg_create();
+    jpeg_set_jpeg_filename(struct jpeg *img_jpeg);
+
+
+
+}
