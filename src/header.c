3fdc6d2bb292fb4e4641cfe69a6bca2e135374c7
Auger <arthur.auger@grenoble-inp.org>
2022-05-03 14:55:43 +0200

    petit changement

--- src/header.c
+++ src/header.c
@@ -25,10 +25,7 @@ struct jpeg write_header_file(
     jpeg_set_image_height(struct jpeg *img_jpeg, uint32_t image_height);
     jpeg_set_image_width(struct jpeg *img_jpeg, uint32_t image_width);
     jpeg_set_nb_components(struct jpeg *img_jpeg, uint8_t nb_components);
-    jpeg_set_sampling_factor(struct jpeg * img_jpeg, enum color_component cc, enum direction dir, uint8_t sampling factor);
-    jpeg_set_huffman_table(struct jpeg, *img_jpeg, enum sample_type adcd, enum color_component cc, struct huff_table *htable);
+    jpeg_set_sampling_factor(struct jpeg *img_jpeg, enum color_component cc, enum direction dir, uint8_t sampling factor);
+    jpeg_set_huffman_table(struct jpeg *img_jpeg, enum sample_type adcd, enum color_component cc, struct huff_table *htable);
     jpeg_set_quantization_table(struct jpeg *img_jpeg, enum color_component cc, uint8_t *qtable)
-
-
-
 }
