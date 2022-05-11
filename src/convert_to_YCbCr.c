08abf52e8045cec1ca3898c09165268d744876a6
Arthur Lebeurrier <Arthur.Lebeurrier@grenoble-inp.org>
2022-05-11 02:31:25 +0200

    convertRGB

--- src/convert_to_YCbCr.c
+++ src/convert_to_YCbCr.c
@@ -31,6 +31,29 @@ struct image_YCbCr *convert_YCbCr(struct image_mcu *p_mcu){
     return p_ycbcr;
 }
 
+struct image_YCbCr *convert_YCbCr_RGB(struct image_mcu_rgb *p_mcu){
+    /*Prend en entrée le découpage MCU et transforme les RGB en YCbCr.
+    La valeur retournée sera un pointeur vers l'image convertie en YCbCr et toujours découpée en MCU.
+    Cette fonction traite dans un premier temps le cas où l'image est en noir et blanc (P5), on pourra l'améliorer à base de if.*/
+
+    struct image_YCbCr *p_ycbcr = creation_YCbCr(p_mcu);
+
+    for(uint32_t i = 0; i < p_ycbcr->nmcu; i++){
+        for(uint8_t j = 0; j<64; j++){
+            struct YCbCr *new_ycbcr= calloc(1, sizeof(struct YCbCr));
+            uint8_t cR = p_mcu->l_mcu[i][j]->R;
+            uint8_t cG = p_mcu->l_mcu[i][j]->G;
+            uint8_t cB = p_mcu->l_mcu[i][j]->B;
+            new_ycbcr->Y = round(0.299 * cR + 0.587 * cG + 0.114 * cB);
+            new_ycbcr->Cb = round(-0.1687 * cR + -0.3313 * cG + 0.5 * cB + 128);
+            new_ycbcr->Cr = round(0.5 * cR + -0.4187 * cG + -0.0813 * cB + 128);
+            p_ycbcr->l_ycbcr[i][j] = new_ycbcr;
+        }
+    }
+    return p_ycbcr;
+}
+
+
 struct image_YCbCr *creation_YCbCr(struct image_mcu *p_mcu){
     /*Création d'un YCbCr avec initialisation des valeurs*/
 
