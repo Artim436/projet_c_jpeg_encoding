59a860fcf9fd00689c24490e198bdaad39d8cc9e
Arthur lebeurrier <Arthur.Lebeurrier@grenoble-inp.org>
2022-05-17 15:14:13 +0200

    timer

--- src/test.c
+++ src/test.c
@@ -12,8 +12,7 @@
 #include <encoding.h>
 #include <write_jpeg_file.h>
 #include <nettoyage.h>
-#include <downsampling.h>
-#include <controle_arguments.h>
+#include <time.h>
 
 
 
@@ -39,8 +38,8 @@ int main (int argc, char **argv) {
 
         process_file(mcu->ppm_filename, mcu, mcu_rgb); //Lecture du fichier et récupération des données
         if(mcu->type_pgm[1] == '5'){
-            /*Nous sommes rentrés dans le cas où nous avons un fichier P5 et donc des composantes en noir et blanc.*/
-
+            clock_t start = clock();
+            //Cas où le fichier est en noir et blanc
             //affiche_details_image(mcu, mcu->ppm_filename); // On affiche les données de l'image
 
             /*Analyse des arguments passés en lignes de commandes*/
@@ -63,33 +62,31 @@ int main (int argc, char **argv) {
 
             write_jpeg_Y(mcu);
             //clean_main_mcu(mcu);
+            clock_t end = clock();
+            double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
+            printf(" %f\n", time_spent);
         }
         else{
-            analyse_arg_sub(pos_des_arg, mcu_rgb, argv);
-
+            clock_t start = clock();
             //Cas où le fichier est en noir et blanc
-            //affiche_details_image_rgb_sub(mcu_rgb); // On affiche les données de l'image
-            // printf("hauteur :  %u    largeur : %u\n",mcu_rgb->height, mcu_rgb->width );
-            struct image_mcu_rgb_sub *img_mcu = decoupe_mcu_rgb_sub(mcu_rgb); //On découpe l'image en blocs de 8x8
-            
+            affiche_details_image_rgb(mcu_rgb, mcu_rgb->ppm_filename); // On affiche les données de l'image
+            struct image_mcu_rgb *img_mcu_rgb = decoupe_mcu_8x8_rgb(mcu_rgb); //On découpe l'image en blocs de 8x8
+            affiche_img_mcu_rgb(img_mcu_rgb);
 
-            //affiche_img_mcu_rgb_sub(img_mcu, mcu_rgb->sampling_factor[0], mcu_rgb->sampling_factor[1]);
-            struct image_YCbCr_sub *p_ycbcr = convert_YCbCr_RGB_sub(img_mcu, mcu_rgb);
-
-            //afficher_YCbCr_sub(p_ycbcr);
-
-            fonction_rgb_sub(mcu_rgb, p_ycbcr);
-
-
-            clean_image_mcu_sub(img_mcu, mcu_rgb->sampling_factor[0], mcu_rgb->sampling_factor[1]);
-
-            clean_image_YCbCr_sub(p_ycbcr);
-
-
-            //affiche_bloc_rgb_sub(mcu_rgb);
-
-            write_jpeg_rgb_sub(mcu_rgb);
-            clean_main_mcu_sub(mcu_rgb);
+            struct image_YCbCr *p_ycbcr = convert_YCbCr_RGB(img_mcu_rgb);
+            printf("On sort de YCbCr. \n");
+            //afficher_YCbCr_rgb(p_ycbcr);
+            fonction_RGB(mcu_rgb, p_ycbcr);
+            //printf("On est sortie de fonction_RGB\n");
+            //clean_image_mcu(img_mcu);
+            //clean_image_YCbCr(p_ycbcr);
+            // printf(" on est l61\n");
+            //affiche_bloc_rgb(mcu_rgb);
+            // printf("On rentre dans write_jpeg : \n");
+            write_jpeg_Y_RGB(mcu_rgb);
+            clock_t end = clock();
+            double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
+            printf(" %f\n", time_spent);
         }
     } else {
         printf("Il faut passer en paramètre le nom d'un fichier image valide \n");
